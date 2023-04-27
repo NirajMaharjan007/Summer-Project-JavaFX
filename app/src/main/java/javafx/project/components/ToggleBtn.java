ba6c8732b2cbec5a39f9070aa7d7511c4a17bf97
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-04-27 23:42:48 +0545

    Updated

--- app/src/main/java/javafx/project/components/ToggleBtn.java
+++ app/src/main/java/javafx/project/components/ToggleBtn.java
@@ -2,7 +2,7 @@ package javafx.project.components;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.geometry.Pos;
-// import javafx.scene.paint.Color;
+import javafx.scene.paint.Color;
 import javafx.scene.text.TextAlignment;
 
 public class ToggleBtn extends MFXButton {
@@ -14,6 +14,14 @@ public class ToggleBtn extends MFXButton {
     }
 
     private void init() {
+        this.setRippleAnimateBackground(true);
+        this.setRippleAnimationSpeed(0.70);
+        this.setRippleBackgroundOpacity(0.35);
+        this.setRippleRadius(25);
+        this.setTextAlignment(TextAlignment.CENTER);
+        this.setRippleColor(Color.web("#cdcdcd"));
+        this.setFocusTraversable(false);
+        this.setFocused(false);
         this.setTextAlignment(TextAlignment.JUSTIFY);
         this.setFocusTraversable(true);
         this.setFocused(true);
