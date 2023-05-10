24329be4befc391ea10cce58e958f655bf2a30b4
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-05-10 22:47:49 +0545

    added content and do update later

--- app/src/main/java/javafx/project/database/EmpDatabase.java
+++ app/src/main/java/javafx/project/database/EmpDatabase.java
@@ -1,6 +1,7 @@
 package javafx.project.database;
 
 import java.sql.*;
+import java.util.*;
 
 public class EmpDatabase {
     private final Connection connection = Database.getConnection();
@@ -9,7 +10,10 @@ public class EmpDatabase {
 
     private AdminDatabase admin = AdminDatabase.getInstance();
 
+    private final int adminId;
+
     private EmpDatabase() {
+        adminId = admin.getId();
     }
 
     public static EmpDatabase getInstance() {
@@ -21,7 +25,6 @@ public class EmpDatabase {
 
     public int count() {
         try {
-            int adminId = admin.getId();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement
                     .executeQuery("SELECT COUNT(*) AS emp_count FROM employees where admin_id=" + adminId);
@@ -33,4 +36,29 @@ public class EmpDatabase {
             return -1;
         }
     }
+
+    public List<Map<String, Object>> getData() {
+        List<Map<String, Object>> data = new ArrayList<>();
+        try {
+            Statement stmt = connection.createStatement();
+            String sql = "SELECT * FROM employees where admin_id=" + adminId;
+            ResultSet rs = stmt.executeQuery(sql);
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            while (rs.next()) {
+                Map<String, Object> row = new HashMap<>();
+                for (int i = 1; i <= columnCount; i++) {
+                    String columnName = metaData.getColumnName(i);
+                    Object value = rs.getObject(i);
+                    row.put(columnName, value);
+                }
+                data.add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
 }
--- app/src/main/java/javafx/project/modules/EmployeeModule.java
+++ app/src/main/java/javafx/project/modules/EmployeeModule.java
@@ -1,11 +1,16 @@
 package javafx.project.modules;
 
+import io.github.palexdev.materialfx.controls.*;
+
 import javafx.geometry.Insets;
+import javafx.project.database.EmpDatabase;
 import javafx.project.enuma.MainStyle;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 
 public class EmployeeModule extends VBox {
+    private EmpDatabase empData = EmpDatabase.getInstance();
+
     public EmployeeModule() {
         super();
         super.setSpacing(16);
@@ -16,12 +21,43 @@ public class EmployeeModule extends VBox {
     }
 
     private void init() {
-        Label header1 = new Label("EmployeeModule");
         this.getStylesheets().add(MainStyle.ALT_STYLESHEET.getLocation());
+
+        Label header1 = new Label("EmployeeModule");
+
+        VBox box = new VBox(12);
+
+        EmployeeTable table = new EmployeeTable();
+
+        box.getChildren().add(table);
 
         header1.getStyleClass().add("header1");
 
-        this.getChildren().add(header1);
+        this.getChildren().addAll(header1, box);
+    }
+
+    private class EmployeeTable extends MFXTableView<String> {
+        public EmployeeTable() {
+            super();
+
+            this.init();
+        }
+
+        private void init() {
+            this.setMinWidth(Double.MIN_NORMAL);
+            this.setMaxWidth(Double.MAX_VALUE);
+            this.setPrefWidth(800);
+
+            MFXTableColumn<String> nameColumn = new MFXTableColumn<>("Name", false);
+            MFXTableColumn<String> surnameColumn = new MFXTableColumn<>("Surname", false);
+            MFXTableColumn<String> ageColumn = new MFXTableColumn<>("Age", false);
+
+            getTableColumns().add(nameColumn);
+            getTableColumns().add(surnameColumn);
+            getTableColumns().add(ageColumn);
+
+            this.autosizeColumn(8);
+        }
     }
 
 }
