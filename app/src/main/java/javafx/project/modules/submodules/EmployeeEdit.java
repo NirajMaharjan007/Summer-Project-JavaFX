a2181c82790c22f4a99b2daa06e601a7f8ab0fdb
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-05-19 10:10:09 +0545

    Added vbox and padding

--- app/src/main/java/javafx/project/modules/submodules/EmployeeEdit.java
+++ app/src/main/java/javafx/project/modules/submodules/EmployeeEdit.java
@@ -3,12 +3,13 @@ package javafx.project.modules.submodules;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.project.components.*;
 import javafx.project.database.*;
 
 public class EmployeeEdit extends BorderPane {
-    private int id;
+    // private int id;
 
     private EmpDatabase data = new EmpDatabase(AdminDatabase.getInstance().getId());
 
@@ -22,13 +23,20 @@ public class EmployeeEdit extends BorderPane {
 
     private void init() {
         VBox box = new VBox(16);
+        VBox left_Vbox = new VBox(8);
+
         MainTextField name = new MainTextField("above");
         name.setFloatingText("Enter a employee name");
 
+        box.setPadding(new Insets(8, 12, 4, 16));
         box.setAlignment(Pos.TOP_CENTER);
         box.getChildren().addAll(name);
 
-        this.setLeft(new Label("Hello"));
+        left_Vbox.setPadding(new Insets(16));
+        left_Vbox.setAlignment(Pos.CENTER);
+        left_Vbox.getChildren().addAll(new Label("Hello World"));
+
+        this.setLeft(left_Vbox);
         this.setCenter(box);
     }
 }
