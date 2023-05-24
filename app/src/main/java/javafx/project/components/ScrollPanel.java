e2e60c77c720fc5dd02767e24f8e480e21974e5e
NirajMaharjan007 <niraj.mhrjn770@gmail.com>
2023-05-24 23:30:25 +0545

    updated components

--- app/src/main/java/javafx/project/components/ScrollPanel.java
+++ app/src/main/java/javafx/project/components/ScrollPanel.java
@@ -2,13 +2,20 @@ package javafx.project.components;
 
 import io.github.palexdev.materialfx.controls.MFXScrollPane;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
 
 public class ScrollPanel extends MFXScrollPane {
     public ScrollPanel() {
         super();
-        super.autosize();
         super.setStyle("-fx-background-color: #fafafa;");
+        this.init();
 
+        super.autosize();
+    }
+
+    public ScrollPanel(Node node) {
+        super(node);
+        super.autosize();
         this.init();
     }
 
