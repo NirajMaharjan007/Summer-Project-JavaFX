c46b5d680bfc99b8b4d53de35afb2a6419de61e1
niraj.mhrjn770 <niraj.mhrjn770@gmail.com>
2023-07-25 21:22:45 +0545

    Database update
    Changed database

--- app/src/main/java/javafx/project/database/Database.java
+++ app/src/main/java/javafx/project/database/Database.java
@@ -6,6 +6,9 @@ import java.sql.DriverManager;
 public class Database {
     private static Connection conn;
 
+    private static final String host = "jdbc:mysql://sql6.freemysqlhosting.net:3306/sql6635251",
+            username = "sql6635251", password = "QydUFrBAgz";
+
     private Database() {
     }
 
@@ -13,10 +16,7 @@ public class Database {
         if (conn == null) {
             try {
                 Class.forName("com.mysql.cj.jdbc.Driver");
-                conn = DriverManager.getConnection(
-                        "jdbc:mysql://bfgj93vkpswbuk9ozeav-mysql.services.clever-cloud.com:3306/bfgj93vkpswbuk9ozeav",
-                        "u6lzhdcupvy4tqcu",
-                        "QNALcQd7InDiAGwGSfSG");
+                conn = DriverManager.getConnection(host, username, password);
             } catch (Exception e) {
                 System.err.println("Database.getConnection(); => " + e.getMessage());
             }
@@ -26,11 +26,17 @@ public class Database {
 }
 
 /*
- * 
+ * 1.
  * conn = DriverManager.getConnection(
  * "jdbc:mysql://localhost:3306/hr_data",
  * "root",
  * ""
  * );
+ * 
+ * 2.
+ * conn = DriverManager.getConnection(
+ * "jdbc:mysql://bfgj93vkpswbuk9ozeav-mysql.services.clever-cloud.com:3306/bfgj93vkpswbuk9ozeav",
+ * "u6lzhdcupvy4tqcu",
+ * "QNALcQd7InDiAGwGSfSG");
  * 
  */
